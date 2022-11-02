5a9decf02273367c610fcda5bd3df281493c976d
kjminn <kjminn2001@gmail.com>
2022-11-02 13:12:59 +0900

    전체 Data 출력

--- src/book/view/BookSearchView.java
+++ src/book/view/BookSearchView.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
@@ -21,14 +22,60 @@ public class BookSearchView extends JPanel {
 	JTextField textSearch;
 	JButton btnSearch;
 	String searchWord = "";
+	JPanel panN;
 	
 	public BookSearchView() {
 		setLayout(new BorderLayout());
 		lbl = new JLabel("검색어: ");
 		textSearch = new JTextField(20);
 		btnSearch = new JButton("검색");
+		panN = new JPanel();
+		panN.add(lbl);
+		panN.add(textSearch);
+		panN.add(btnSearch);		
 	}
-
+	
+	public void initView() {
+		model = new DefaultTableModel(header, bookVOList.size()) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
+		table = new JTable(model);
+		
+		JScrollPane scroll = new JScrollPane(table);
+		
+		putSearchResult();		
+		
+		add("North", panN);
+		add("Center", scroll);
+	}
+	
+//	DefaultTableModel에 도서정보들을 설정한다.
+	public void putSearchResult() {
+		BookVO vo = null;
+		for (int i = 0; i < bookVOList.size(); i++) {
+			vo = bookVOList.get(i);
+//			i:행번호 0:열번호
+			model.setValueAt(vo.getIsbn(), i, 0);
+			model.setValueAt(vo.getName(), i, 1);
+			model.setValueAt(vo.getPublish(), i, 2);
+			model.setValueAt(vo.getAuthor(), i, 3);
+			model.setValueAt(vo.getPrice(), i, 4);
+			model.setValueAt(vo.getCategory(), i, 5);
+		}
+	}
+	
+	public String getSearchWord() {
+		searchWord = textSearch.getText();
+		return searchWord;
+	}
+	
+	public void setBookVOList(ArrayList<BookVO> bookVOList) {
+		this.bookVOList = bookVOList;
+	}
 }
 
 
