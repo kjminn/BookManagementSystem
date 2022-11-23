7a14724201f501612fb2adfeee4846214d4a32e5
kjminn <kjminn2001@gmail.com>
2022-11-23 14:20:15 +0900

    도서 수정, 삭제

--- src/book/controller/BookController.java
+++ src/book/controller/BookController.java
@@ -2,36 +2,41 @@ package book.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 import book.dao.BookDAO;
 import book.dao.JDBCConnector;
 import book.view.BookInsertView;
 import book.view.BookSearchView;
+import book.view.BookUpdateView;
 import book.vo.BookVO;
 
 public class BookController extends JFrame {
 	BookSearchView searchPan;
 	BookDAO dao;
 	ArrayList<BookVO> bookVOList;
-	Connection con;
 	JComboBox<String> combo;
 	BookInsertView insertPan;
+	BookUpdateView updatePan;
+	JTable table;
 	
 	public BookController() {
 		JTabbedPane tab = new JTabbedPane(JTabbedPane.TOP);
-		con = JDBCConnector.getCon();
 		searchPan = new BookSearchView();
 		
 		combo = searchPan.getCombo();
 		dao = new BookDAO();
-		bookVOList = dao.select(con, searchPan.getSearchWord(), combo.getSelectedIndex());
+		bookVOList = dao.select(searchPan.getSearchWord(), combo.getSelectedIndex());
 		searchPan.setBookVOList(bookVOList);
 		searchPan.initView();
 		
@@ -40,14 +45,25 @@ public class BookController extends JFrame {
 		
 //		도서 추가 화면
 		insertPan = new BookInsertView();
-		bookVOList = dao.select(con, "", 0);
+		bookVOList = dao.select("", 0);
 		insertPan.setBookVOList(bookVOList);
 		insertPan.initView();
 		JButton btnAdd = insertPan.getBtnAdd();
 		btnAdd.addActionListener(btnAddL);		
 		
+//		도서 수정 화면	
+		updatePan = new BookUpdateView();
+		bookVOList = dao.select("", 0);
+		updatePan.setBookVOList(bookVOList);
+		updatePan.initView();
+		JButton btnUpdate = updatePan.getBtnUpdate();
+		btnUpdate.addActionListener(btnUpdateL);
+		table = updatePan.getTable();	
+		table.addMouseListener(tableL);
+		
 		tab.add("도서검색", searchPan);
 		tab.add("도서추가", insertPan);
+		tab.add("도서수정", updatePan);
 		
 		add(tab);
 		setTitle("도서관리시스템");
@@ -55,12 +71,33 @@ public class BookController extends JFrame {
 		setBounds(500, 300, 600, 500);
 		setVisible(true);
 	}
+	
+	MouseAdapter tableL = new MouseAdapter() {
+		public void mouseClicked(MouseEvent e) {
+			if(e.getClickCount() == 2) {
+				int result = JOptionPane.showConfirmDialog(BookController.this, "정말로 삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+				if(result == 0) {//예: 0, 아니오:1
+					BookVO vo = updatePan.neededUpdateData();
+					dao.delete(vo);
+					bookVOList = dao.select("", 0);
+					updatePan.setBookVOList(bookVOList);
+					updatePan.putSearchResult();
+				}
+			}
+			
+			if(e.getClickCount() == 1) {
+				int rowIndex = table.getSelectedRow();
+				updatePan.setTextField(rowIndex);
+			}
+
+		}
+	};
 
 	ActionListener btnL = new ActionListener() {
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			bookVOList = dao.select(con, searchPan.getSearchWord(), combo.getSelectedIndex());
+			bookVOList = dao.select(searchPan.getSearchWord(), combo.getSelectedIndex());
 			searchPan.setBookVOList(bookVOList);
 			searchPan.putSearchResult();
 		}
@@ -71,11 +108,23 @@ public class BookController extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			BookVO vo = insertPan.neededInsertData();
-			dao.insert(con, vo);
-			bookVOList = dao.select(con, searchPan.getSearchWord(), combo.getSelectedIndex());
+			dao.insert(vo);
+			bookVOList = dao.select("", 0);
 			insertPan.setBookVOList(bookVOList);
 			insertPan.putSearchResult();
 			insertPan.initInsertData();
+		}
+	};
+	
+	ActionListener btnUpdateL = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			BookVO vo = updatePan.neededUpdateData();
+			dao.update(vo);
+			bookVOList = dao.select("", 0);
+			updatePan.setBookVOList(bookVOList);
+			updatePan.putSearchResult();
 		}
 	};
 	
--- src/book/dao/BookDAO.java
+++ src/book/dao/BookDAO.java
@@ -12,15 +12,18 @@ public class BookDAO {
 	ArrayList<BookVO> bookVOList;  
 	
 	
-	public ArrayList<BookVO> select(Connection con, String searchWord, int selectedIndex){
+	public ArrayList<BookVO> select(String searchWord, int selectedIndex){
+		Connection con = JDBCConnector.getCon();
+		ResultSet rs = null; 
+		PreparedStatement pstmt = null;
 		bookVOList = new ArrayList<BookVO>();
 		String[] columnName = {"name", "publish", "author"};
-		System.out.println(selectedIndex);
+//		System.out.println(selectedIndex);
 		try {
 			String sql = "select isbn, name, publish, author, price, category_name from book, category where book.category=category.category_id and "+columnName[selectedIndex]+" like ?";
-			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, "%"+searchWord+"%");
-			ResultSet rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				BookVO vo = new BookVO();
 				vo.setIsbn(rs.getInt("isbn"));
@@ -34,16 +37,34 @@ public class BookDAO {
 		
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(con != null) {
+					con.close();
+				}
+			}catch (SQLException e) {
+				System.out.println("select close할 때 문제 발생됨");
+			}
 		} 
 		
 		
 		return bookVOList;
 	}
 	
-	public void insert(Connection con, BookVO vo) {
+	public void insert(BookVO vo) {
+		Connection con = JDBCConnector.getCon();
+		PreparedStatement pstmt = null;
 		String sql = "insert into book values(?, ?, ?, ?, ?, ?);";
 		try {
-			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, vo.getIsbn());
 			pstmt.setString(2, vo.getName());
 			pstmt.setString(3, vo.getPublish());
@@ -71,14 +92,108 @@ public class BookDAO {
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(con != null) {
+					con.close();
+				}
+			}catch (SQLException e) {
+				System.out.println("insert close할 때 문제 발생됨");
+			}
+		} 
 		
 	}
 	
+	public void update(BookVO vo) {
+		Connection con = JDBCConnector.getCon();
+		String sql = "update book set name=?, publish=?, author=?, price=?, category=? where isbn=?;";
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPublish());
+			pstmt.setString(3, vo.getAuthor());
+			pstmt.setInt(4, vo.getPrice());
+			int categoryId = 0;
+			switch (vo.getCategoryName()) {
+			case "IT도서":
+				categoryId = 10;
+				break;
+			case "소설":
+				categoryId = 20;
+				break;
+			case "비소설":
+				categoryId = 30;
+				break;
+			case "경제":
+				categoryId = 40;
+				break;
+			case "사회":
+				categoryId = 50;
+				break;				
+			}
+			pstmt.setInt(5, categoryId);
+			pstmt.setInt(6, vo.getIsbn());
+			
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("update 실행 오류");
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(con != null) {
+					con.close();
+				}
+			}catch (SQLException e) {
+				System.out.println("update close할 때 문제 발생됨");
+			}
+		} 
+		
+	}
 	
-	
-	
+	public void delete(BookVO vo) {
+		Connection con = JDBCConnector.getCon();
+		PreparedStatement pstmt = null;
+		String sql = "delete from book where isbn=?;";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, vo.getIsbn());
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println("delete 할 때 오류발생함");
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(con != null) {
+					con.close();
+				}
+			}catch (SQLException e) {
+				System.out.println("delete close할 때 문제 발생됨");
+			}
+		} 
+	}
 	
 	
 
 }
+
+
+
+
+
+
+
+
